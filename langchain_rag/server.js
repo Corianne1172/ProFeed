39031d0f647a6371831bb144b9e7381ff2865592
Braydin Larson <130601331+Blars98@users.noreply.github.com>
2025-07-10 00:19:24 -0500

    Update server.js

--- langchain_rag/server.js
+++ langchain_rag/server.js
@@ -15,6 +15,11 @@ app.use('/api', assignmentRoutes);
 const PORT = process.env.PORT || 3001;
 app.use(express.static(path.join(process.cwd(), 'frontend')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'frontend', 'login.html'));
+});
+
+
 app.listen(PORT, () => {
   console.log(`Express server running on http://localhost:${PORT}`);
 });
